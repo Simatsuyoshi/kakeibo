31e1207081d23ef28a94d334dc9dc47971cfbfa0
tsuyoshi.shima <acc46trera@gmail.com>
2022-03-16 08:01:18 +0000

    group

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
    @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
    @orders = current_user.costs.where(date: @month.all_month).order(date: :DESC)  
    #@categories = @orders.costs.find(params[:category_id])
-   @category_content = @orders.group(:category_id).sum(:cost).sort_by { |_, v| _ }.reverse.to_h
+   @category_content = @orders.group('category_id').sum(:cost).sort_by { |_, v| _ }.reverse.to_h
    @category = @orders.where(category_id: params[:id])
    @category_name = Category.find(params[:id])
   end
