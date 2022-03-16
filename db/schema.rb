c3ab2811bd2ea29b4a70aa2645d8dbc90b76fcaa
tsuyoshi.shima <acc46trera@gmail.com>
2022-03-16 07:36:57 +0000

    img

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 2022_03_04_135413) do
   create_table "costs", charset: "utf8mb4", force: :cascade do |t|
     t.date "date"
     t.string "content"
-    t.bigint "cost"
+    t.bigint "cost", null: false
     t.bigint "user_id", null: false
     t.bigint "category_id"
-    t.bigint "kind_id", null: false
+    t.bigint "kind_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_costs_on_category_id"
